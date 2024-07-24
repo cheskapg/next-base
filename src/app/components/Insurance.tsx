21e9225f8c073fe45ae8e9a155a81cafe9d44abf
nachochz <fpalmagil@jairosoftcom.onmicrosoft.com>
2024-07-24 15:18:17 +0800

    loader

--- src/app/components/Insurance.tsx
+++ src/app/components/Insurance.tsx
@@ -595,11 +595,11 @@ export default function Insurance() {
         <button
           id="validate"
           type="button"
-          onClick={handleWithInsurance}
+          onClick={handleValidation}
           className={`bg-spruce-4 w-full rounded-3xl py-2 text-center text-white relative`}
         >
           {isLoading ? (
-            <span className="absolute inset-0 flex items-center justify-center">
+            <span className="absolute inset-0 h-full w-full flex items-center justify-center">
               <svg
                 className="animate-spin h-5 w-5 mr-3"
                 xmlns="http://www.w3.org/2000/svg"
@@ -623,11 +623,34 @@ export default function Insurance() {
               Loading...
             </span>
           ) : (
-            'Validate Insurance'
+            // 'Validate Insurance'
+            <span className="absolute inset-0  flex items-center justify-center">
+              <svg
+                className="animate-spin h-5 w-5 mr-3"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+              Loading...
+            </span>
           )}
         </button>
       </div>
-      
+
         {/* Action */}
         <div
           className={`flex items-end gap-4 ${values.insuranceSubscriber === '1' || values.insuranceSubscriber === undefined ? 'h-full' : 'h-full'}`}
