e42849f6e7312def5493b1d0321110adb3253f6d
nachochz <fpalmagil@jairosoftcom.onmicrosoft.com>
2024-08-07 17:53:56 +0800

    initial values update

--- src/app/components/QuestionsForms/BehavioralQuestions.tsx
+++ src/app/components/QuestionsForms/BehavioralQuestions.tsx
@@ -63,10 +63,20 @@ const BehavorialQuestions = () => {
 //     return values;
 //   }, {});
 
-const initialValues = questions.reduce((values, question) => {
-    values[question.id] = ''; // Initialize each question with an empty string
-    return values;
-  }, {});
+  // Explicitly define initial values for each question
+  const initialValues = {
+    question1: '',  // Concatenated string of selected checkboxes
+    question2: '',  // Textbox value
+    question3: '',  // Concatenated string of selected checkboxes
+    question4: '',  // Concatenated string of selected checkboxes
+    question5: '',  // Concatenated string of selected checkboxes
+    question6: '',  // Concatenated string of selected checkboxes
+    question7: '',  // Concatenated string of selected checkboxes
+    question8: '',  // Concatenated string of selected checkboxes
+    question9: '',  // Concatenated string of selected checkboxes
+    question10: '',  // Concatenated string of selected checkboxes
+   
+  };
 
 
   const handleCheckboxChange = (
